81d0201d270bd7e5112c0f6ceb0bad1921b4a12f
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-04-01 01:52:55 +0330

    move functions

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { PhotoService } from '../services/photo.service';
 import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 
 @Component({
@@ -10,18 +10,9 @@ import { CommonModule } from '@angular/common';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent, CommonModule],
+  imports: [IonicModule, ExploreContainerComponent, CommonModule, RouterLink],
 })
 export class Tab1Page {
-  photos = this.photoService.photos;
-  constructor(public photoService: PhotoService) { }
-
-  async ngOnInit() {
-    await this.photoService.loadSaved();
-  }
-
-  addPhotoToGallery() {
-    this.photoService.addNewToGallery();
-  }
+ 
 
 }
